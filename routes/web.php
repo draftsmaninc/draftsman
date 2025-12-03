5688fbd4d4f34ba5b12cfcf84cafe7e83d3307f0
Ron Northrip <ron.northrip@gmail.com>
2025-12-03 11:08:43 -0500

    Addd draftsman _next route with passthru for any files there

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,41 @@
 
 use Draftsman\Draftsman\Http\Controllers\ApiV1\ModelsController;
 use Draftsman\Draftsman\Http\Controllers\ApiV1\RelationsController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('draftsman')->group(function () {
+    Route::prefix('_next')->group(function ($request) {
+        $uri = Request::getRequestUri();
+        $prefix = '/draftsman/_next/';
+        if (substr($uri, 0, strlen($prefix)) === $prefix) {
+            $file = __DIR__.'/../resources/views/_next/' . substr($uri, strlen($prefix), strlen($uri));
+            if (file_exists($file)) {
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                $mime = mime_content_type($file);
+                if ($mime === 'text/plain') {
+                    switch ($ext) {
+                        case 'css':
+                            $mime = 'text/css';
+                            break;
+                        case 'html':
+                            $mime = 'text/html';
+                            break;
+                        case 'js':
+                            $mime = 'application/javascript';
+                            break;
+                        case 'json':
+                            $mime = 'application/json';
+                            break;
+                    }
+                }
+                header('Content-type: '.$mime);
+                readfile($file);
+                return;
+            }
+        }
+        abort(404);
+    });
     Route::prefix('api')->group(function () {
         // API Routes...
         Route::get('models/presorted', [ModelsController::class, 'presorted']);
