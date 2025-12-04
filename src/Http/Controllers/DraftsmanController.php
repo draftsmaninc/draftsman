c71eab110cd2177201167acb27705391a5dbef06
Ron Northrip <ron.northrip@gmail.com>
2025-12-03 23:38:59 -0500

    Added next controller

--- src/Http/Controllers/DraftsmanController.php
+++ src/Http/Controllers/DraftsmanController.php
@@ -8,6 +8,7 @@ use Illuminate\Routing\Controller;
 class DraftsmanController extends Controller
 {
     protected $index_file = 'resources/front/index.html';
+    protected $next_dir = 'resources/front/_next/';
     protected $package_root_path = '/../../../';
 
     /**
@@ -17,10 +18,43 @@ class DraftsmanController extends Controller
     {
         $file = __DIR__.$this->osSafe($this->package_root_path.$this->index_file);
         $front = file_get_contents($file);
-        $pattern = '/(href|src)=(["\'])(\/)(_next)\//im';
-        $replacement = '/$1=$2$3draftsman$3$4/';
-        $front = preg_replace($pattern, $replacement, $front);
         return $front;
+    }
+
+    /**
+     * Pass the font end nextjs resources.
+     */
+    public function next(Request $request)
+    {
+        $uri = $request->path();
+        $prefix = 'draftsman/_next/';
+        if (substr($uri, 0, strlen($prefix)) === $prefix) {
+            $uri = $this->next_dir. substr($uri, strlen($prefix));
+            $file = __DIR__.$this->osSafe($this->package_root_path.$uri);
+            if (file_exists($file)) {
+                $content = file_get_contents($file);
+                $ext = pathinfo($file, PATHINFO_EXTENSION);
+                $mime = mime_content_type($file);
+                if ($mime === 'text/plain') {
+                    switch ($ext) {
+                        case 'css':
+                            $mime = 'text/css';
+                            break;
+                        case 'html':
+                            $mime = 'text/html';
+                            break;
+                        case 'js':
+                            $mime = 'application/javascript';
+                            break;
+                        case 'json':
+                            $mime = 'application/json';
+                            break;
+                    }
+                    return response()->file($file, ['Content-Type' => $mime]);
+                }
+            }
+        }
+//        abort(404);
     }
 
     protected function osSafe(string $path)
