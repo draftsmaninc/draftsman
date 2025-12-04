1f11fd4f605e2200867c6951f24ed49456afff29
Ron Northrip <ron.northrip@gmail.com>
2025-12-03 23:39:36 -0500

    Updated web routes to point to draftmsn next controller

--- routes/web.php
+++ routes/web.php
@@ -8,34 +8,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('draftsman')->group(function () {
     Route::prefix('_next')->group(function ($request) {
-        $uri = Request::getRequestUri();
-        $prefix = '/draftsman/_next/';
-        if (substr($uri, 0, strlen($prefix)) === $prefix) {
-            $file = '/../resources/views/_next/'.substr($uri, strlen($prefix), strlen($uri));
-            $file = __DIR__.implode(DIRECTORY_SEPARATOR, explode('/', $file));
-            if (file_exists($file)) {
-                $content = file_get_contents($file);
-                $ext = pathinfo($file, PATHINFO_EXTENSION);
-                $mime = mime_content_type($file);
-                if ($mime === 'text/plain') {
-                    switch ($ext) {
-                        case 'css':
-                            $mime = 'text/css';
-                            break;
-                        case 'html':
-                            $mime = 'text/html';
-                            break;
-                        case 'js':
-                            $mime = 'application/javascript';
-                            break;
-                        case 'json':
-                            $mime = 'application/json';
-                            break;
-                    }
-                }
-                response($content)->header('Content-Type', $mime)->send();
-            }
-        }
+        Route::get(
+            '/{slug0?}/{slug1?}/{slug2?}/{slug3?}/{slug4?}/{slug5?}/{slug6?}/{slug7?}/{slug8?}/{slug9?}',
+            [DraftsmanController::class, 'next']
+        )->name('draftsman.next');
     });
     Route::prefix('api')->group(function () {
         // API Routes...
