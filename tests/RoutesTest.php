9062530eb579ffb8995aaf837ace85403382cf51
Ron Northrip <ron.northrip@gmail.com>
2025-12-04 00:04:51 -0500

    Assert we get 200 for js and css

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -5,3 +5,13 @@ it('defaults to the layout', function () {
     $response->assertStatus(200);
     $response->assertSee('Draftsman');
 });
+
+it('gets a js file from _next ', function () {
+    $response = $this->get('/draftsman/_next/static/chunks/1a258343-4e35aaf719b73d9c.js');
+    $response->assertStatus(200);
+});
+
+it('gets a css file from _next ', function () {
+    $response = $this->get('/draftsman/_next/static/css/8cfc3c2c8dcafd94.css');
+    $response->assertStatus(200);
+});
