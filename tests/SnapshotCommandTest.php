9ebbb6abbfe9b4bf1aa8a47d43553954be40fe35
ZacHiler <79065463+ZacHiler@users.noreply.github.com>
2025-12-15 19:51:01 -0500

    added initial config structure

--- tests/SnapshotCommandTest.php
+++ tests/SnapshotCommandTest.php
@@ -9,7 +9,7 @@ it('saves snapshot to default config path when no --path is provided', function 
     if (File::exists($defaultDir)) {
         File::deleteDirectory($defaultDir);
     }
-    config(['draftsman.snapshot_path' => $defaultDir]);
+    config(['draftsman.config.snapshot_path' => $defaultDir]);
 
     // Act
     $exit = Artisan::call('draftsman:snapshot');
